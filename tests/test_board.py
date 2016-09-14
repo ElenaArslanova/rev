c8dbd311def859c088dd3332e32d5f9847c9e9a9
Elena Arslanova <contilen@gmail.com>
2016-09-14 19:07:56 +0500

    tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,30 +1,4 @@
-import kit
-import unittest
-from settings import *
-
-
-class TestBoard(unittest.TestCase):
-    def test_set_start_cells(self):
-        board = kit.Board()
-        self.assertIsNot(board.board[SIZE // 2 - 1][SIZE // 2 - 1], EMPTY)
-        self.assertIsNot(board.board[SIZE // 2 - 1][SIZE // 2], EMPTY)
-        self.assertIsNot(board.board[SIZE // 2][SIZE // 2 - 1], EMPTY)
-        self.assertIsNot(board.board[SIZE // 2][SIZE // 2], EMPTY)
-
-    def test_mark_move_in_direction_west(self):
-        board = kit.Board()
-        board.mark_move_in_direction(BLACK, board.board[4][4], WEST)
-        self.assertEqual(board.board[2][4].can_be_taken, True)
-        self.assertEqual(board.board[4][2].can_be_taken, False)
-        self.assertEqual(board.board[5][5].can_be_taken, False)
-        board.clear_moves()
-
-    def test_valid_moves_start(self):
-        board = kit.Board()
-        board.mark_valid_moves(WHITE)
-        for i, j in zip([2, 3, 4, 5], [3, 2, 5, 4]):
-            self.assertEqual(board.board[i][j].can_be_taken, True)
-
+import kit66666666666666666666
 
     def test_valid_moves_corner(self):
         board = kit.Board()
